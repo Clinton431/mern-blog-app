218358a70b7bacb1eb405a7153857631408fb910
Clinton431 <clintonnyakoe@gmail.com>
2025-11-25 19:00:35 +0300

    Images fix

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,5 +1,8 @@
 import { formatISO9075 } from "date-fns";
 import { Link } from "react-router-dom";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
 export default function Post({
   _id,
   title,
@@ -12,7 +15,7 @@ export default function Post({
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={"http://localhost:4000/" + cover} alt="" />
+          <img src={`${API_URL}/` + cover} alt="" />
         </Link>
       </div>
       <div className="texts">
